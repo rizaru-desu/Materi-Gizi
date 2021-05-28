c770e752536ccbf062cb5457e67dd531bd49b922
rizaru-desu <rizaru_desu@yahoo.com>
2021-05-28 13:33:59 +0700

    upload project gizi

---
+++ assets/Api/ApiFirebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4P2YhudRFytIKy0NGLFFm_qFwNL1tuuo",
+  authDomain: "materi-gizi.firebaseapp.com",
+  databaseURL: "https://materi-gizi-default-rtdb.firebaseio.com",
+  projectId: "materi-gizi",
+  storageBucket: "materi-gizi.appspot.com",
+  messagingSenderId: "187425842632",
+  appId: "1:187425842632:web:f22dd4d4db43b779148cfd",
+  measurementId: "G-300CXJG2VL",
+};
